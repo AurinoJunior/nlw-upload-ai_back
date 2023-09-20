b12b25d04e290152d46df813839a8020aa17f3d2
Aurino Geraldo <aurinoaj@gmail.com>
2023-09-20 10:43:56 -0300

    feat: create generate ai route

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
 import { getAllPrompts } from "./routes/get-all-prompts";
 import { uploadVideo } from "./routes/upload-video";
 import { createTranscription } from "./routes/create-transcription";
+import { generateAICompletion } from "./routes/generate-ai-completion";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 app.register(getAllPrompts);
 app.register(uploadVideo);
 app.register(createTranscription);
+app.register(generateAICompletion);
 
 app
   .listen({
